d38851e36c145c02d7f7cf3713f8cfe9f0c6d333
Tolulope Olusakin <tolu.olusakin@yahoo.ie>
2020-06-04 18:56:49 -0700

    Scaffolding components

---
+++ src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './LoadMoreBtn';
+
+const LoadMoreBtn = (props) => {
+    return (
+        <div>
+            MovieThumb
+        </div>
+    )
+}
+
+export default LoadMoreBtn
